81a0139f1ad2f4701c02cf31845202e7fe5994bb
Vibhas K <vibhaskamalhome@gmail.com>
2021-10-22 22:56:14 +0530

    Made the converted text field reactive to user's live inputs

--- src/App.js
+++ src/App.js
@@ -5,71 +5,62 @@ function App() {
   let [displayText, setDisplayText] = useState('');
   let [convertedText, setconvertedText] = useState('');
 
+  function handleInputChange(e){
+    setUserInput(e.target.value);
+    convertToBoolean(e.target.value);
+  }
+
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(userInput);
+    // console.log(userInput);
     setDisplayText(userInput);
     let str = userInput.length;
-    console.log(str);
+    // console.log(str);
     convertToBoolean(userInput);
   };
 
   function handleClear() {
     setUserInput('');
     setDisplayText('');
+    setconvertedText('');
     console.clear();
   };
 
   function convertToBoolean(text) {
     text = text.toLowerCase();
     let len = text.length;
-    // for (let i = 0; i < len; i++){
-    //   console.log("char: ", text.charAt(i));
-    // }
-    // read the text
-    // separate text into array
-    // separate and, or, not
-    // replace and, or, not with their symbols
-
     let i = 0;
     while (i < len) {
       if (text.charAt(i) === ' ') {
-        // Checking for 'and'
         if ((i + 1) < len && text.charAt(i + 1) === 'a' && (i + 2) < len && text.charAt(i + 2) === 'n'
           && (i + 3) < len && text.charAt(i + 3) === 'd' && (i + 4) < len && text.charAt(i + 4) === ' ') {
-          // text = text.substr(i+2, i+4)
-          text = text.slice(0, i) + ' &' + text.slice(i + 4);
-
-          console.log(`text: ${text}`);
-          console.log('and');
-          // i = i + 3;
+          text = text.slice(0, i) + ' ∧' + text.slice(i + 4);
+          // console.log(`text: ${text}`);
+          // console.log('and');
         }
         else if ((i + 1) < len && text.charAt(i + 1) === 'n' && (i + 2) < len && text.charAt(i + 2) === 'o'
           && (i + 3) < len && text.charAt(i + 3) === 't' && (i + 4) < len && text.charAt(i + 4) === ' ') {
-          text = text.slice(0, i) + ' ~' + text.slice(i + 4);
-          console.log(`text: ${text}`);
-          console.log('not');
-          // i = i + 3;
+          text = text.slice(0, i) + ' ¬' + text.slice(i + 4);
+          // console.log(`text: ${text}`);
+          // console.log('not');
         }
         else if ((i + 1) < len && text.charAt(i + 1) === 'o' && (i + 2) < len && text.charAt(i + 2) === 'r'
           && (i + 3) < len && text.charAt(i + 3) === ' ') {
-          text = text.slice(0, i) + ' ||' + text.slice(i + 3);
-          console.log(`text: ${text}`);
-          console.log('or');
-          // i = i + 2;
+          text = text.slice(0, i) + ' ∨' + text.slice(i + 3);
+          // console.log(`text: ${text}`);
+          // console.log('or');
         }
       }
       i++;
-      // else {
-      //   i++;
-      // }
     }
-  }
+    setconvertedText(text);
+  };
 
   return (
     <div className="App">
       <label>User input: </label>
-      <input type="text" value={userInput} onChange={event => setUserInput(event.target.value)} />
+      {/* <input type="text" value={userInput} onChange={event => setUserInput(event.target.value)} /> */}
+      <input type="text" value={userInput} onChange={event => handleInputChange(event)} />
 
       <br />
       <button onClick={handleSubmit}>
