9ef0029a82b0dca9a3db0bdf699362dd400f4659
Vibhas K <vibhaskamalhome@gmail.com>
2021-10-23 01:42:46 +0530

    Organised imports and removed comments

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { useState } from 'react';
-import { convertTextToBoolean } from './services/convertTextToBoolean';
-import './App.css';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import { convertTextToBoolean } from './services/convertTextToBoolean';
+import './App.css';
 
 const placeholderText = 'E.g. a and b or c not d';
 
@@ -35,34 +35,6 @@ function App() {
 
   return (
     <div className="App">
-      {/*  
-      <Typography variant="h5" gutterBottom component="div">
-        Enter text
-      </Typography>
-      <br />
-      <TextField
-        id="outlined-basic"
-        label={placeholderText}
-        value={userInput}
-        onChange={event => handleInputChange(event)}
-        variant="outlined" />
-      <br /><br />
-      <div>
-        <Button variant="contained" onClick={handleSubmit}>Submit</Button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <Button variant="contained" onClick={handleClear}>Clear</Button>
-      </div>
-      <br />
-      <Typography variant="h5" gutterBottom component="div">
-        Converted text
-      </Typography>
-      <br />
-      <TextField
-        id="outlined-basic"
-        value={convertedText}
-        readOnly={true}
-        variant="outlined" />
-        */}
       <Grid container spacing={0}>
         <Grid item xs={5}>
           <Typography variant="h5" gutterBottom component="div">
