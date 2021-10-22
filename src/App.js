88901eaf79bc9c710997b28c122fd4c8becd06da
Vibhas K <vibhaskamalhome@gmail.com>
2021-10-23 01:41:26 +0530

    Utilising Material design grid

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { convertTextToBoolean } from './services/convertTextToBoolean';
 import './App.css';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -33,6 +35,7 @@ function App() {
 
   return (
     <div className="App">
+      {/*  
       <Typography variant="h5" gutterBottom component="div">
         Enter text
       </Typography>
@@ -59,6 +62,40 @@ function App() {
         value={convertedText}
         readOnly={true}
         variant="outlined" />
+        */}
+      <Grid container spacing={0}>
+        <Grid item xs={5}>
+          <Typography variant="h5" gutterBottom component="div">
+            Enter text
+          </Typography>
+          <br />
+          <TextField
+            sx={{ width: '50ch' }}
+            id="outlined-basic"
+            label={placeholderText}
+            value={userInput}
+            onChange={event => handleInputChange(event)}
+            variant="outlined" />
+        </Grid>
+        <Grid item xs={2} style={{ 'margin-top': '50px' }}>
+          <Stack spacing={2}>
+            <Button variant="contained" onClick={handleSubmit}>Submit</Button>
+            <Button variant="contained" onClick={handleClear}>Clear</Button>
+          </Stack>
+        </Grid>
+        <Grid item xs={5}>
+          <Typography variant="h5" gutterBottom component="div">
+            Converted text
+          </Typography>
+          <br />
+          <TextField
+            sx={{ width: '50ch' }}
+            id="outlined-basic"
+            value={convertedText}
+            readOnly={true}
+            variant="outlined" />
+        </Grid>
+      </Grid>
     </div>
   );
 }
