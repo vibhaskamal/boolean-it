fb913ad169babfd68cf433c7422ff1d2c16926f9
Vibhas K <vibhaskamalhome@gmail.com>
2021-10-22 22:13:58 +0530

    Successfully able to check for and not

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
 import { useState } from 'react';
 import './App.css';
 function App() {
-  let [input, setInput] = useState('');
+  let [userInput, setUserInput] = useState('');
+  let [displayText, setDisplayText] = useState('');
+  let [convertedText, setconvertedText] = useState('');
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(input);
+    console.log(userInput);
+    setDisplayText(userInput);
+    let str = userInput.length;
+    console.log(str);
+    convertToBoolean(userInput);
   };
 
   function handleClear() {
-    setInput('');
+    setUserInput('');
+    setDisplayText('');
+    console.clear();
   };
+
+  function convertToBoolean(text) {
+    text = text.toLowerCase();
+    let len = text.length;
+    // for (let i = 0; i < len; i++){
+    //   console.log("char: ", text.charAt(i));
+    // }
+    // read the text
+    // separate text into array
+    // separate and, or, not
+    // replace and, or, not with their symbols
+
+    let i = 0;
+    while (i < len) {
+      if (text.charAt(i) === ' ') {
+        // Checking for 'and'
+        if ((i + 1) < len && text.charAt(i + 1) === 'a' && (i + 2) < len && text.charAt(i + 2) === 'n'
+          && (i + 3) < len && text.charAt(i + 3) === 'd' && (i + 4) < len && text.charAt(i + 4) === ' ') {
+          console.log('and');
+        }
+        else if ((i + 1) < len && text.charAt(i + 1) === 'n' && (i + 2) < len && text.charAt(i + 2) === 'o'
+          && (i + 3) < len && text.charAt(i + 3) === 't' && (i + 4) < len && text.charAt(i + 4) === ' ') {
+          console.log('not');
+        }
+      };
+      i++;
+    }
+  }
 
   return (
     <div className="App">
-      <label>
-        Name: <br />
-        <input type="text" value={input} onChange={event => setInput(event.target.value)} />
-      </label>
+      <label>User input: </label>
+      <input type="text" value={userInput} onChange={event => setUserInput(event.target.value)} />
+
       <br />
       <button onClick={handleSubmit}>
         Submit
@@ -25,6 +60,10 @@ function App() {
       <button onClick={handleClear}>
         Clear
       </button>
+      <br />
+      <label>Display text: </label> <input type="text" value={displayText} readOnly={true} />
+      <br />
+      <label>Converted text: </label> <input type="text" value={convertedText} readOnly={true} />
     </div>
   );
 }
