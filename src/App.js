c1fcf92f12c5dd59d5db88edc858a88f2ccad208
Vibhas K <vibhaskamalhome@gmail.com>
2021-10-24 00:05:49 +0530

    Added app bar with links

--- src/App.js
+++ src/App.js
@@ -8,10 +8,40 @@ import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Link from '@mui/material/Link';
 import { convertTextToBoolean } from './services/convertTextToBoolean';
 import './App.css';
 
 const placeholderText = 'E.g. a and b or c not d';
+
+function ButtonAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
 
 function App() {
   let [userInput, setUserInput] = useState('');
@@ -35,7 +65,24 @@ function App() {
 
   return (
     <div className="App">
-      <Grid container spacing={0}>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar variant="dense">
+            <Typography variant="h6" color="inherit" component="div">
+              <Link href="#" underline="none"  style={{color: 'white'}}>
+                Boolean-it
+              </Link>
+            </Typography>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <Typography variant="h6" color="inherit" component="div">
+              <Link href="#" underline="none"  style={{color: 'white'}}>
+                Instructions
+              </Link>
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Grid container spacing={0} style={{'margin-top': '40px'}}>
         <Grid item xs={5}>
           <Typography variant="h5" gutterBottom component="div">
             Enter text
@@ -51,7 +98,7 @@ function App() {
         </Grid>
         <Grid item xs={2} style={{ 'margin-top': '50px' }}>
           <Stack spacing={2}>
-            <Button variant="contained" onClick={handleSubmit}>Submit</Button>
+            <Button variant="contained" onClick={handleSubmit}>Convert</Button>
             <Button variant="contained" onClick={handleClear}>Clear</Button>
           </Stack>
         </Grid>
