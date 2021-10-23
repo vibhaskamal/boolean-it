3b0038fce94057c32acb833b493614bb77074fad
Vibhas K <vibhaskamalhome@gmail.com>
2021-10-24 00:29:17 +0550

    Removed unused imports and added git automation script

--- src/App.js
+++ src/App.js
@@ -11,37 +11,11 @@ import Typography from '@mui/material/Typography';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import Link from '@mui/material/Link';
 import { convertTextToBoolean } from './services/convertTextToBoolean';
 import './App.css';
 
 const placeholderText = 'E.g. a and b or c not d';
-
-function ButtonAppBar() {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
-}
 
 function App() {
   let [userInput, setUserInput] = useState('');
